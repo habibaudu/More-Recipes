f2dd6a1269d49b103e719419e33fb193c17f9ccf
Habib <auduhabib1990@gmail.com>
2017-10-03 08:45:44 +0100

    Changed the content of my .travis.yml file

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
-// import express from "express";
-// import logger from "morgan";
-// import bodyParser from "body-parser";
+ import express from "express";
+ import logger from "morgan";
+ import bodyParser from "body-parser";
 
-const express = require("express");
-const logger = require("morgan");
-const bodyParser= require("body-parser");
+// const express = require("express");
+// const logger = require("morgan");
+// const bodyParser= require("body-parser");
 
 
 // express app set up
--- server/bin/www.js
+++ server/bin/www.js
@@ -6,4 +6,9 @@ const port = parseInt(process.env.PORT, 10) || 5000;
 app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port);
+server.listen(port,(err)=>{
+    if(!err)
+        console.log(`Server runing on port  ${port}`);
+    else console.log(err);
+ 
+});
